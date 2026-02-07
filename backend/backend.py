1d50b69db26d770d618eb8337145269d071d297a
Michelle Wei <michellewei@Michelles-MacBook-Air.local>
2026-02-06 23:52:58 -0500

    add everything

--- backend/backend.py
+++ backend/backend.py
@@ -22,6 +22,8 @@ supabase_url = os.getenv('SUPABASE_URL')
 supabase_key = os.getenv('SUPABASE_KEY')
 supabase: Client = create_client(supabase_url, supabase_key)
 
+
+
 # ------------------------ FastAPI ----------------------------
 app = FastAPI(
     title="Swole.ai",
